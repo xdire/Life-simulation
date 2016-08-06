b3d4949c32f6392ca6f5deae893793cc3c905756
Anton Repin <dire.one@gmail.com>
2016-08-06 17:40:06 -0400

    Initial Commit

---
+++ Game Of Life/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game Of Life
+//
+//  Created by Anton Repin on 8/6/16.
+//  Copyright © 2016 Anton Repin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Game Of LifeTests/Game_Of_LifeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Game_Of_LifeTests.swift
+//  Game Of LifeTests
+//
+//  Created by Anton Repin on 8/6/16.
+//  Copyright © 2016 Anton Repin. All rights reserved.
+//
+
+import XCTest
+@testable import Game_Of_Life
+
+class Game_Of_LifeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
